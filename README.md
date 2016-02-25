c0adeace704aeb8ade829bd090bb8d4d906fb302
dignifiedquire <dignifiedquire@gmail.com>
2016-02-25 12:44:18 +0100

    Add object.patch.appendData

--- README.md
+++ README.md
@@ -4,8 +4,7 @@
 
 ## Running
 
-Make sure your `ipfs daemon` is running before running this. Also you need
-node >= 4 and npm >= 3.
+You need will node >= 4 and npm >= 3. Also this assumes that ipfs@0.4 is installed in your `$GOPATH/bin`.
 
 ```bash
 $ npm install
--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 'use strict'
 
-const ipfs = require('ipfs-api')()
+require('espower-loader')({
+  cwd: process.cwd(),
+  pattern: 'lib/**/*.js'
+})
+const ipfsd = require('ipfsd-ctl')
 const requireDir = require('require-dir')
 
 const exec = require('./lib/exec')
 const generate = require('./lib/generate')
 const OPERATIONS = requireDir('./lib/operations', {camelCase: true})
 
-exec(OPERATIONS, ipfs, generate(OPERATIONS, 10))
+const daemon = () => new Promise((resolve, reject) => {
+  ipfsd.disposable((err, node) => {
+    if (err) return reject(err)
+    node.startDaemon((err, ipfs) => {
+      if (err) return reject(err)
+      resolve(ipfs)
+    })
+  })
+})
+daemon()
+  .then((ipfs) => {
+    return exec(OPERATIONS, ipfs, generate(OPERATIONS, 10))
+  })
   .then(() => {
     console.log('SUCCESS')
     process.exit()
   })
   .catch((err) => {
-    console.error('FAILURE')
-    console.error(err)
+    console.error(err.message)
     process.exit(1)
   })
