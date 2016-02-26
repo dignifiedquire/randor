f74481a81e0615619f6308808ca64f1669f3d96d
dignifiedquire <dignifiedquire@gmail.com>
2016-02-26 13:59:13 +0100

    Use streams to allow for unlimited operations count
    Closes #4

--- index.js
+++ index.js
@@ -4,55 +4,37 @@ require('espower-loader')({
   cwd: process.cwd(),
   pattern: 'lib/**/*.js'
 })
-const Promise = require('bluebird')
 const ipfsd = require('ipfsd-ctl')
 const requireDir = require('require-dir')
-const fs = Promise.promisifyAll(require('fs'))
+const _ = require('highland')
+const debug = require('debug')
 
 const exec = require('./lib/exec')
 const generate = require('./lib/generate')
 const OPERATIONS = requireDir('./lib/operations', {camelCase: true})
 
-const daemon = () => new Promise((resolve, reject) => {
-  ipfsd.disposable((err, node) => {
-    if (err) return reject(err)
-    node.startDaemon((err, ipfs) => {
-      if (err) return reject(err)
-      resolve(ipfs)
-    })
+const log = debug('randor:main')
+log.error = debug('randor:error')
+
+const commandCount = 1000
+
+_
+  .wrapCallback(ipfsd.disposable)()
+  .flatMap((node) => {
+    log('Starting daemon')
+    return _.wrapCallback(node.startDaemon.bind(node))()
   })
-})
-
-let commandCount
-let commandList
-
-if (process.argv.length > 2) {
-  const filename = process.argv[2]
-  commandCount = parseInt(filename.split('-')[0], 10)
-  commandList = JSON.parse(fs.readFileSync(filename, 'utf8'))
-} else {
-  commandCount = 100 // * 1000
-  commandList = generate(OPERATIONS, commandCount)
-}
-
-daemon()
-  .then((ipfs) => exec(ipfs, commandList))
-  .then(() => {
-    console.log('SUCCESS')
+  .flatMap((ipfs) => {
+    log('Starting operations')
+    return generate(OPERATIONS).flatMap(exec(ipfs))
+  })
+  .take(commandCount)
+  .errors((err) => {
+    if (err) {
+      log.error(err)
+      process.exit(1)
+    }
+  })
+  .done(() => {
     process.exit()
-  })
-  .catch((err) => {
-    console.log('FAILURE')
-    console.error(err.message)
-    console.error(err.stack)
-
-    const fileName = `${commandCount}-${+new Date()}.json`
-
-    return fs.writeFileAsync(fileName, JSON.stringify(commandList))
-      .then(() => {
-        console.log('Wrote commands %s', fileName)
-      })
-      .finally(() => {
-        process.exit(1)
-      })
   })
